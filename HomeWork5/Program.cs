f8f6ba187f8d0fe18992441fbb26b8e720459ce9
Kate Malanina <katpm07@gmail.com>
2023-02-28 16:27:25 +0300

    Seminar5 done

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+/*
+int CountEven(int[] array)
+{
+    int counter = 0;
+    {
+    for(int i = 0; i < array.Length; i++)
+    if(array[i] % 2 == 0)
+    counter++;
+    }
+return counter;
+}
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i =0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[] newArray = CreateRandomArray(5, 100, 999);
+ShowArray(newArray);
+int result = CountEven(newArray);
+Console.WriteLine($"the number of even elements in the array: {result} ");
+*/
+
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях. [3, 7, 23, 12] -> 19   [-4, -6, 89, 6] -> 0
+
+
+
+
+
+
+// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива. [3 7 22 2 78] -> 76
---
+++ Seminar5/Program.cs
@@ -0,0 +1,189 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. 
+// Найдите сумму отрицательных элементов массива.
+/*
+1 генерирующие методы -сформировать новый массив
+2 аналитические - существующий массив берут и не меняя его выполняет расчеты
+3 преобразующие - существующий массив берут и вносят необходимые изменения
+*/
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i =0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int GetNegativeSum(int[] array)
+{
+    int sum = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0)
+            sum += array[i];
+    }
+
+    return sum;
+}
+
+Console.Write("Input a quantity of elements ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min posible value ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max posible value ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(size, min, max);
+ShowArray(newArray);
+int result = GetNegativeSum(newArray);
+Console.WriteLine($"Sum of negative numbers is " + result);
+*/
+
+// Напишите программу замены элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i =0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[] ChangePosAndNeg(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] *= -1;
+    return array;
+}
+
+Console.Write("Input a quantity of elements ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min posible value ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max posible value ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateRandomArray(size, min, max);
+ShowArray(newArray);
+ShowArray(ChangePosAndNeg(newArray));
+
+*/
+
+//Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i =0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}    
+
+bool FindNumber(int [] array, int number)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        return true;    
+    }
+    return false;
+}
+
+Console.Write("Input a quantity of elements ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min posible value ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max posible value ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] newArray = CreateRandomArray(size, min, max);
+ShowArray(newArray);
+bool result = FindNumber(newArray, number);
+if (result)
+    Console.WriteLine($"Number {number} at the array");
+else
+Console.WriteLine($"Number {number} not on a array");
+*/
+
+// Задайте одномерный массив из m случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [a,b].
+/*
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i =0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int CountNumbers(int [] array, int A, int B)
+{
+    int counter = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= A && array[i] <= B)
+        counter++;
+    }
+    return counter;
+}
+
+Console.Write("Input A ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B ");
+int B = Convert.ToInt32(Console.ReadLine());
+int[] newArray = CreateRandomArray(12, 1, 40);
+ShowArray(newArray);
+
+int result = CountNumbers(newArray,A, B);
+Console.WriteLine($"There are a {result} numbers on a array");
+*/
